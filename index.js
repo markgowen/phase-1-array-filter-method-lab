c6597b22afdda35f6342213d1633a2e7ed679f7c
Mark Gowen <mark.gowen78@gmail.com>
2022-11-06 11:12:09 -0800

    commit tree 5413d2b46f5a8ab3da5a9c237111d0e0810d5a7a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, driver) {
+    return array.filter((e) => {
+        return e.toLowerCase().includes(driver.toLowerCase())
+    });
+}
+
+function fuzzyMatch(array, driver) {
+    return array.filter((e) => {
+        return e.startsWith(driver);
+    });
+}
+
+function matchName(drivers, elements) {
+    return drivers.filter((e) => e.name === elements)
+
+}
